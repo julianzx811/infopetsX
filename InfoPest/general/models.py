5b33a8fd6e0f4e4c0c371c1c223ab54be2460647
thecolombianabortion <julianolomanolox@hotmail.com>
2022-04-24 16:44:23 -0700

    pequeños cambios xd

--- InfoPest/general/models.py
+++ InfoPest/general/models.py
@@ -4,13 +4,14 @@ from django.utils import timezone
 
 class usuarios(models.Model):
     usuario = models.CharField(max_length=200)
-    contrasena = models.CharField(max_length=200)
+    contrasena = models.CharField(max_length=200,null=False,default='no_contraseña')
     email_user = models.CharField(max_length=300, default='corre@hotmail.com')
     sexo = models.CharField(max_length=200, default='no llenado')
     edad = models.IntegerField(default=0)
 
     def __str__(self):
         return self.usuario + '-' + self.contrasena
+
 
 class pets(models.Model):
     dueno_id = models.ForeignKey(usuarios, on_delete=models.CASCADE)
