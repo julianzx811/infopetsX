6004f6cf1c0fd02627e2f8b02478d4748128eeab
thecolombianabortion <julianolomanolox@hotmail.com>
2022-04-29 12:38:39 -0700

    paso entre de parametros entre paginas casi hecho

--- InfoPest/general/urls.py
+++ InfoPest/general/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'general'
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('pag_principal/', views.pag_principal, name='pag_principal'),
+    path('redirect_pag_principal/', views.redirect_pag_principal, name='redirect_pag_principal'),
+    path('pag_principal/<int:id>', views.pag_principal, name='pag_principal'),
     path('registrandome/', views.registrandome, name='registrandome'),
     path('registro/', views.registro, name='registro'),
-    path('pag_principal/new_mascota/<str:mascota>', views.creando_mascota, name='creando_mascota'),
+    path('pag_principal/new_mascota/', views.creando_mascota, name='creando_mascota'),
     path('pag_principal/ver_mascotas/', views.ver_mascotas, name='ver_mascotas'),
 ]
--- InfoPest/general/views.py
+++ InfoPest/general/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render, get_object_or_404
-from .models import usuarios, mascota,info_pets_empresas,veterinaria
+from django.shortcuts import render, reverse, redirect
+from .models import usuarios, mascota,info_pets_empresas, veterinaria
 
 # return render(request, 'polls/index.html', context)
 
 def inicio(request):
     return render(request, 'html/login.html')
 
-def pag_principal(request):
-    context = {}
-    usuario = request.POST.get('uname')
-    contrasena = request.POST.get('psw')
-    usuario_query = usuarios.objects.filter(usuario=usuario)
-    contrasena_query = usuarios.objects.filter(contrasena=contrasena)
-    id =usuarios.objects.filter(usuario=usuario).values_list('id', flat=True)
+def redirect_pag_principal(request):
+    usuario_ = request.POST.get('uname')
+    contrasena_ = request.POST.get('psw')
+    #metodo con post para autenticar de que el usuario exista
+    usuario_query = usuarios.objects.filter(usuario=usuario_)
+    contrasena_query = usuarios.objects.filter(contrasena=contrasena_)
     if usuario_query.exists() and contrasena_query.exists():
-        context['uname'] = usuario
-        context['psw'] = contrasena
-        context['id'] = id
+        id = usuarios.objects.get(usuario=usuario_)
+        return pag_principal(request, id.id)
     else:
+        return pag_principal(request, 0)
+
+def pag_principal(request,id):
+    context = {}
+    if id == 0:
         context['uname'] = 'no'
         context['psw'] = 'no'
-    return render(request, 'html/inicio.html',context)
+    else:
+        usuario = usuarios.objects.get(id=id)
+        context['uname'] = usuario.usuario
+        context['psw'] = usuario.contrasena
+    return render(request,'html/inicio.html',context)
 
 def registrandome(request):
     return render(request, 'html/registro.html')
@@ -38,8 +45,8 @@ def registro(request):
     context['uname'] = usuarioxd
     return render(request, 'html/inicio.html', context)
 
-def creando_mascota(request,mascota):
-    return render(request, 'html/newMascota.html', {'uname':mascota})
+def creando_mascota(request):
+    return render(request, 'html/newMascota.html')
 
 def ver_mascotas(request):
     return render(request, 'html/mascotas.html')
